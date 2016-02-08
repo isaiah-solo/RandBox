e4f8e86145220e462852c031a633548aba3733b6
isayyuhh-s <isaiahsolomon16@gmail.com>
2016-02-07 21:00:03 -0800

    created a working java lib

--- Java/Main.java
+++ Java/Main.java
@@ -1,8 +1,12 @@
 public class Main {
-
    public static void main (String[] args) {
-      
-      
-      
+      RandGen rand = new RandGen("red");
+      rand.add("red");
+      rand.add("red");
+      rand.add("blue");
+      rand.add("green");
+      rand.add("purple");
+
+      System.out.println(rand.pickRand());
    }
 }
--- Java/RandGen.java
+++ Java/RandGen.java
@@ -1,30 +1,76 @@
 import java.util.*;
 
 public class RandGen {
-   private final static float HUNDRED = 100.0;
-   private final static float INIT = 1.0;
-   private HashMap<String, float> map;
+   private final static int HUNDRED = 100;
+   private final static int INIT = 1;
+
+   private int size = 0;
+   private HashMap<String, Integer> map;
 
    public RandGen () {
       this.map = new HashMap<>();
    }
 
+   public RandGen (String elem) {
+      this.map = new HashMap<>();
+      Integer init = new Integer(INIT);
+      this.map.put(elem, init);
+      this.size++;
+   }
+
    public RandGen (List<String> list) {
       this.map = new HashMap<>();
-      for (String str: list) {
-         float check = this.map.get(str);
+      for (String elem: list) {
+         Integer check = this.map.get(elem);
          if (check == null) {
-            float init = INIT;
-            this.map.put(str, init);
+            Integer init = new Integer(INIT);
+            this.map.put(elem, init);
          }
          else {
             check += 1;
          }
+         this.size++;
       }
    }
    
+   public void add (String newElem) {
+      if (this.map.containsKey(newElem)) {
+         Integer newInt = new Integer(this.map.get(newElem).intValue() + 1);
+         this.map.put(newElem, newInt);
+      }
+      else {
+         Integer init = new Integer(INIT);
+         this.map.put(newElem, init);
+      }
+      this.size++;
+   }
    
+   public void delete (String elem) {
+      if (this.map.containsKey(elem)) {
+         this.map.remove(elem);
+         this.size--;
+      }
+   }
    
-   
-   
+   public float checkChance (String elem) {
+      float percent = 0;
+      if (this.map.containsKey(elem)) {
+         percent = ((float) this.map.get(elem) * 100) / ((float) HUNDRED);
+      }
+      return percent;
+   }
+
+   public String pickRand () {
+      Random rand = new Random();
+      int choice = rand.nextInt(this.size) + 1;
+
+      Set<Map.Entry<String, Integer>> set = map.entrySet();
+      for (Map.Entry<String, Integer> elem: set) {
+         choice -= elem.getValue().intValue();
+         if (choice <= 0) {
+            return elem.getKey();
+         }
+      }
+      return "ERROR";
+   }
 }
