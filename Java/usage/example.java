bb7821fa58f400d149d0ee3f8f1f4b2bdcf8e8ac
isayyuhh-s <isaiahsolomon16@gmail.com>
2016-02-23 00:40:27 -0800

    updated java usage

--- Java/usage/example.java
+++ Java/usage/example.java
@@ -4,47 +4,50 @@ import randbox.*;
 public class example {
    public static void main (String[] args) {
       // Variables to declare 
-      Object[] array = {(Object) "blue", (Object) "green", (Object) "purple"};
-      Object redString = (Object) "red";
-      int green = 0, red = 0, blue = 0, purple = 0;
+      String red = new String("red");
+      String blue = new String("blue");
+      String green = new String("green");
+      String purple = new String("purple");
+      String[] array = {blue, green, purple};
+      int g = 0, r = 0, b = 0, p = 0;
       
       // Initialize new Randbox using an array of elements
       RandBox rb = new RandBox(array);
       
       // Add elements to RandBox
-      rb.add(redString, 5);
+      rb.add(red, 5);
       
       // Delete elements from RandBox
-      rb.delete(redString, 2);
+      rb.delete(red, 2);
 
       // Displaying probability of selected elements from RandBox
-      System.out.println("Red = " + String.valueOf(
-                         rb.probability("red")));
-      System.out.println("Blue = " + String.valueOf(
-                         rb.probability("blue")) + "\n");
+      System.out.println("red = " + String.valueOf(
+                         rb.probability(red)));
+      System.out.println("blue = " + String.valueOf(
+                         rb.probability(blue)) + "\n");
       
       // Displaying size of RandBox
-      System.out.println("Size = " + String.valueOf(
+      System.out.println("size = " + String.valueOf(
                          rb.size()) + "\n");
       
       // Pick random element from RandBox
-      System.out.println("Random 1 string:");
-      System.out.println(rb.pick() + "\n");
+      System.out.println("random 1 string:");
+      System.out.println((String) rb.pick() + "\n");
       
       // Pick multiple random elements from RandBox
       List<Object> list = rb.multPick(20);
       
       for (Object elem: list) {
-         if (((String) elem).equals("red")) red++;
-         if (((String) elem).equals("blue")) blue++;
-         if (((String) elem).equals("green")) green++;
-         if (((String) elem).equals("purple")) purple++;
+         if (((String) elem).equals("red")) r++;
+         if (((String) elem).equals("blue")) b++;
+         if (((String) elem).equals("green")) g++;
+         if (((String) elem).equals("purple")) p++;
       }
       
-      System.out.println("Random 20 strings:");
-      System.out.println("red: " + String.valueOf(red));
-      System.out.println("blue: " + String.valueOf(blue));
-      System.out.println("green: " + String.valueOf(green));
-      System.out.println("purple: " + String.valueOf(purple));
+      System.out.println("random 20 strings:");
+      System.out.println("red: " + String.valueOf(r));
+      System.out.println("blue: " + String.valueOf(b));
+      System.out.println("green: " + String.valueOf(g));
+      System.out.println("purple: " + String.valueOf(p));
    }
 }
